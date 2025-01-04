bcde0852728ae4af0dfe88ed774fc62de866e2b6
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-01-04 17:24:53 -0600

    added base for the stopwatch

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,12 @@
+use std::io::Write;
+use termion::{input::TermRead, raw::IntoRawMode};
+
 enum ApiState {
     Init,
     Sw,
     Cd,
 }
+
 pub struct Api {
     state: ApiState,
 }
@@ -40,7 +44,45 @@ impl Api {
                 println!("Please give a valid argument, use -h for list of arguments.");
                 std::process::exit(1);
             }
-            ApiState::Sw => {}
+            ApiState::Sw => {
+                let (tx, rx) = std::sync::mpsc::channel::<&str>();
+                let _stdout = std::io::stdout().into_raw_mode().unwrap();
+
+                std::thread::spawn(move || {
+                    let stdin = std::io::stdin();
+                    for c in stdin.keys() {
+                        match c.unwrap() {
+                            termion::event::Key::Esc => {
+                                tx.send("ESC").unwrap();
+                                std::process::exit(1);
+                            }
+                            _ => {}
+                        }
+                    }
+                });
+
+                let sw_start = std::time::Instant::now();
+                loop {
+                    print!(
+                        "\rTime: {}s",
+                        sw_start.elapsed().as_millis() as f32 / 1000f32
+                    );
+                    std::io::stdout().flush().unwrap();
+                    match rx.try_recv() {
+                        Ok("ESC") => {
+                            //println!("");
+                            print!("\n\rExited successfully.\n\r");
+                            std::io::stdout().flush().unwrap();
+                            break;
+                        }
+                        Ok(_) => {
+                            println!("help");
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
             ApiState::Cd => {}
         }
     }
