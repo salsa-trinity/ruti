22b04b1496604347aef7a84b5e2a4f37e19e230d
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-01-06 10:46:47 -0600

    moved sw code to a new file
    also changed a bit of the sw code to make each thread on a different
    function

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,3 @@
-use std::io::Write;
-use termion::{input::TermRead, raw::IntoRawMode};
-
 enum ApiState {
     Init,
     Sw,
@@ -44,66 +41,7 @@ impl Api {
                 println!("Please give a valid argument, use -h for list of arguments.");
                 std::process::exit(1);
             }
-            ApiState::Sw => {
-                let (tx, rx) = std::sync::mpsc::channel::<&str>();
-                let _stdout = std::io::stdout().into_raw_mode().unwrap();
-
-                std::thread::spawn(move || {
-                    let stdin = std::io::stdin();
-                    for c in stdin.keys() {
-                        match c.unwrap() {
-                            termion::event::Key::Esc => {
-                                tx.send("ESC").unwrap();
-                                std::process::exit(1);
-                            }
-                            termion::event::Key::Char(' ') => {
-                                tx.send("PAUSE").unwrap();
-                            }
-                            termion::event::Key::Char('\n') => {
-                                tx.send("LAP").unwrap();
-                            }
-                            _ => {}
-                        }
-                    }
-                });
-
-                let mut total_time = std::time::Duration::from_secs(0);
-                let mut is_running = true;
-                let mut lap_count = 0;
-                loop {
-                    let loop_time = std::time::Instant::now();
-
-                    match rx.try_recv() {
-                        Ok("ESC") => {
-                            break;
-                        }
-                        Ok("PAUSE") => {
-                            is_running = !is_running;
-                        }
-                        Ok("LAP") => {
-                            println!(
-                                "\rLap {}: {}s",
-                                lap_count,
-                                total_time.as_millis() as f32 / 1000f32,
-                            );
-                            lap_count += 1;
-                            if !is_running {
-                                print!("\rTime: {}s", total_time.as_millis() as f32 / 1000f32,)
-                            }
-                            std::io::stdout().flush().unwrap();
-                        }
-                        _ => {}
-                    }
-
-                    // end of loop
-                    std::thread::sleep(std::time::Duration::from_micros(100));
-                    if is_running {
-                        print!("\rTime: {}s", total_time.as_millis() as f32 / 1000f32);
-                        std::io::stdout().flush().unwrap();
-                        total_time += loop_time.elapsed();
-                    }
-                }
-            }
+            ApiState::Sw => crate::sw::main(),
             ApiState::Cd => {}
         }
     }
---
+++ src/sw.rs
@@ -0,0 +1,73 @@
+use std::io::Write;
+use termion::{input::TermRead, raw::IntoRawMode};
+
+pub fn main() {
+    let (tx, rx) = std::sync::mpsc::channel::<&str>();
+    let _stdout = std::io::stdout().into_raw_mode().unwrap();
+    // i don't know why, but changing the name to _ breaks it :(
+
+    std::thread::spawn(move || {
+        crate::sw::listener_thread(tx);
+    });
+
+    crate::sw::loop_thread(rx);
+}
+
+fn listener_thread(tx: std::sync::mpsc::Sender<&str>) {
+    let stdin = std::io::stdin();
+    for c in stdin.keys() {
+        match c.unwrap() {
+            termion::event::Key::Esc => {
+                tx.send("ESC").unwrap();
+                std::process::exit(1);
+            }
+            termion::event::Key::Char(' ') => {
+                tx.send("PAUSE").unwrap();
+            }
+            termion::event::Key::Char('\n') => {
+                tx.send("LAP").unwrap();
+            }
+            _ => {}
+        }
+    }
+}
+
+fn loop_thread(rx: std::sync::mpsc::Receiver<&str>) {
+    let mut total_time = std::time::Duration::from_secs(0);
+    let mut is_running = true;
+    let mut lap_count = 0;
+
+    loop {
+        let loop_time = std::time::Instant::now();
+
+        match rx.try_recv() {
+            Ok("ESC") => {
+                break;
+            }
+            Ok("PAUSE") => {
+                is_running = !is_running;
+            }
+            Ok("LAP") => {
+                println!(
+                    "\rLap {}: {}s",
+                    lap_count,
+                    total_time.as_millis() as f32 / 1000f32,
+                );
+                lap_count += 1;
+                if !is_running {
+                    print!("\rTime: {}s", total_time.as_millis() as f32 / 1000f32,)
+                }
+                std::io::stdout().flush().unwrap();
+            }
+            _ => {}
+        }
+
+        // end of loop
+        std::thread::sleep(std::time::Duration::from_micros(1000));
+        if is_running {
+            print!("\rTime: {}s", total_time.as_millis() as f32 / 1000f32);
+            std::io::stdout().flush().unwrap();
+            total_time += loop_time.elapsed();
+        }
+    }
+}
