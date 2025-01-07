e9ec60f38937386f9db5e71e86a56a6ef404862b
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-01-06 19:41:44 -0600

    added time parsing for cd and _cd mode
    NOTE: _cd should not be used by a user, it is meant to be used for the
    future implementation for detaching a countdown

--- src/api.rs
+++ src/api.rs
@@ -3,16 +3,23 @@ enum ApiState {
     Init,
     Sw,
     Cd,
+    BgCd,
 }
 
 #[derive(Clone)]
 pub struct ApiFlags {
     pub pl: bool,
+    pub len: u64,
+    pub len_defined: bool,
 }
 
 impl ApiFlags {
     fn new() -> ApiFlags {
-        ApiFlags { pl: false }
+        ApiFlags {
+            pl: false,
+            len: 0,
+            len_defined: false,
+        }
     }
 }
 
@@ -41,20 +48,51 @@ impl Api {
                     self.state = ApiState::Cd;
                     state_change_counter += 1;
                 }
+                "_cd" => {
+                    self.state = ApiState::BgCd;
+                    state_change_counter += 1;
+                }
                 "-h" | "--help" => Api::flag_h(),
                 "-pl" | "--pause-lap" => self.flags.pl = true,
                 _ => {
-                    println!("ERROR: Not a valid argument, use -h for list of arguments.");
-                    std::process::exit(1);
+                    self.flags.len = match flag.trim().parse() {
+                        Ok(num) => {
+                            self.flags.len_defined = true;
+                            num
+                        }
+                        Err(_) => {
+                            println!("ERROR: Not a valid argument, use -h for list of arguments.");
+                            std::process::exit(1);
+                        }
+                    };
                 }
             }
         }
+
+        // make sure only one mode is used at the time
         if state_change_counter >= 2 {
             println!("ERROR: only a single mode can be used at the time.");
             std::process::exit(1);
         }
-        if self.flags.pl && self.state != ApiState::Sw {
+        // ensure -pl flag is only used with sw
+        else if self.flags.pl && self.state != ApiState::Sw {
             println!("ERROR: -pl flag must only be used with the sw command.");
+            std::process::exit(1);
+        }
+        // make sure length is only specified with cd
+        else if self.state != ApiState::BgCd
+            && self.state != ApiState::Cd
+            && self.flags.len_defined
+        {
+            println!("ERROR: length must not be specified with a command other than cd.");
+            std::process::exit(1);
+        }
+        // ensure length is given to cd
+        else if self.state != ApiState::BgCd
+            && self.state != ApiState::Cd
+            && !self.flags.len_defined
+        {
+            println!("ERROR: must specify a length for cd command.");
             std::process::exit(1);
         }
     }
@@ -66,6 +104,7 @@ impl Api {
                 std::process::exit(1);
             }
             ApiState::Sw => crate::sw::main(self.flags.clone()),
+            ApiState::BgCd => crate::bgcd::main(),
             ApiState::Cd => {}
         }
     }
