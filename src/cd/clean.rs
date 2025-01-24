363c8f7eff135e91f43a0a086dd66446322f22d1
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-01-23 19:58:16 -0600

    added cd killing to clean and polished it

--- src/cd/clean.rs
+++ src/cd/clean.rs
@@ -1,15 +1,27 @@
 use crate::args::Args;
 use directories::ProjectDirs;
-use std::fs;
+use std::{fs, process};
 
 pub fn cd_clean_main(args: Args) {
     let data_dir = ProjectDirs::from("com", "github", "ruti").unwrap();
     let data_dir = data_dir.data_local_dir();
     let mut count = 0;
     for file in data_dir.read_dir().unwrap() {
-        fs::remove_file(file.unwrap().path()).unwrap();
+        let file = file.unwrap();
         count += 1;
+        if file.file_name() != "pn" {
+            let _ = process::Command::new("kill")
+                .arg("-9")
+                .arg(&file.file_name())
+                .stdout(process::Stdio::null())
+                .stderr(process::Stdio::null())
+                .spawn();
+        }
+        fs::remove_file(file.path()).unwrap();
     }
-    // TODO: kill running countdowns
-    println!("Successfully deleted {} files.", count);
+    match count {
+        0 => println!("Countdowns already cleaned."),
+        1 => println!("Successfully cleaned 1 countdown."),
+        _ => println!("Successfully cleaned {} countdowns.", count),
+    }
 }
