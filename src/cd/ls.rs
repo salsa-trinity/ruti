2214c2170d91e16e36854a488491e9e84d798653
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-01-31 18:39:53 -0600

    cd ls now shows the pn of each countdown

--- src/cd/ls.rs
+++ src/cd/ls.rs
@@ -1,24 +1,37 @@
 use crate::args::Args;
 use directories::ProjectDirs;
-use std::{fs, os::unix::process::CommandExt, process};
+use std::{fs, process};
 
 pub fn cd_ls_main(args: Args) {
-    // TODO: show the identifier for that cd, either pn or pid
     let data_path = ProjectDirs::from("com", "github", "ruti").unwrap();
     let data_path = data_path.data_local_dir();
     let mut no_file = true;
     for file in fs::read_dir(&data_path).unwrap() {
         let file = file.unwrap();
         if file.file_name() != "dn" {
+            for (i, line) in fs::read_to_string(file.path()).unwrap().lines().enumerate() {
+                match (i, line) {
+                    (2, l) => print!("{}: ", l),
+                    _ => {}
+                }
+            }
             no_file = false;
-            process::Command::new("ruti")
-                .arg("cd")
-                .arg("st")
-                .arg("-s")
-                .arg("-p")
-                .arg(file.file_name())
-                .spawn()
-                .unwrap();
+            // TODO: make it print in a grid-like format
+            print!(
+                "{}",
+                String::from_utf8_lossy(
+                    &process::Command::new("ruti")
+                        .arg("cd")
+                        .arg("st")
+                        .arg("-s")
+                        .arg("-n")
+                        .arg("-p")
+                        .arg(file.file_name())
+                        .output()
+                        .unwrap()
+                        .stdout
+                )
+            );
         }
     }
     if no_file {
