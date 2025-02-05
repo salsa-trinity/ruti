bce9a1d99b6eada3e214a29bd6468226ea66ecb3
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-02-04 19:55:13 -0600

    added better cd interface tooling

--- src/cd_iface.rs
+++ src/cd_iface.rs
@@ -1,15 +1,22 @@
 use std::{fs, path::Path};
 
+use directories::ProjectDirs;
+
 pub struct CdIface {
     pub total: f64,
     pub target: f64,
     pub pn: String,
     pub dn: bool,
+    pub pid: u32,
 }
 
 impl CdIface {
-    pub fn new(path: &Path) -> CdIface {
+    pub fn from_path(path: &Path) -> Option<CdIface> {
         let (mut total, mut target, mut pn, mut dn) = (0f64, 0f64, "", false);
+
+        if fs::read_to_string(&path).is_err() {
+            return None;
+        }
 
         let lines = fs::read_to_string(path).unwrap();
         let lines: Vec<&str> = lines.lines().collect();
@@ -27,11 +34,44 @@ impl CdIface {
                 _ => {}
             }
         }
-        CdIface {
+
+        Some(CdIface {
             total,
             target,
             pn: pn.to_string(),
             dn,
+            pid: path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+                .parse()
+                .unwrap(),
+        })
+    }
+
+    pub fn from_pid(pid: u32) -> Option<CdIface> {
+        let data_path = ProjectDirs::from("com", "github", "ruti").unwrap();
+        let data_path = data_path.data_local_dir();
+
+        CdIface::from_path(&data_path.join(pid.to_string()))
+    }
+
+    pub fn from_pn(pn: &str) -> Option<CdIface> {
+        let data_path = ProjectDirs::from("com", "github", "ruti").unwrap();
+        let data_path = data_path.data_local_dir();
+
+        for file in fs::read_dir(&data_path).unwrap() {
+            let file = file.unwrap();
+
+            if CdIface::from_path(&file.path()).is_some() {
+                if CdIface::from_path(&file.path()).unwrap().pn == pn {
+                    return CdIface::from_path(&file.path());
+                }
+            }
         }
+
+        None
     }
 }
