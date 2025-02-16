712d8e01d9d76489f6c5b48d99c75dc19b8b563d
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-02-16 09:18:28 -0600

    made sw file cleaner

--- src/sw.rs
+++ src/sw.rs
@@ -15,20 +15,20 @@ use termion::{
 pub fn sw_main(args: Args) {
     let (tx, rx) = mpsc::channel::<&str>();
     let tx2 = tx.clone();
-    let raw_mode_handle = io::stdout().into_raw_mode().unwrap();
+    let raw_handle = io::stdout().into_raw_mode().unwrap();
 
-    thread::spawn(move || listener_thread(tx, raw_mode_handle));
+    thread::spawn(move || listener_thread(tx, raw_handle));
 
     loop_thread(rx, tx2, args);
 }
 
-fn listener_thread(tx: mpsc::Sender<&str>, _stdout: RawTerminal<Stdout>) {
+fn listener_thread(tx: mpsc::Sender<&str>, raw_handle: RawTerminal<Stdout>) {
     let stdin = io::stdin();
     for c in stdin.keys() {
         match c.unwrap() {
             Key::Esc => {
                 tx.send("ESC").unwrap();
-                _stdout.suspend_raw_mode().unwrap();
+                raw_handle.suspend_raw_mode().unwrap();
                 process::exit(1);
             }
             Key::Char(' ') => {
@@ -42,7 +42,7 @@ fn listener_thread(tx: mpsc::Sender<&str>, _stdout: RawTerminal<Stdout>) {
     }
 }
 
-fn loop_thread(rx: mpsc::Receiver<&str>, tx2: mpsc::Sender<&str>, args: Args) {
+fn loop_thread(rx: mpsc::Receiver<&str>, tx: mpsc::Sender<&str>, args: Args) {
     let mut total_time = Duration::from_secs(0);
     let mut is_running = true;
     let mut lap_count = 0;
@@ -62,7 +62,7 @@ fn loop_thread(rx: mpsc::Receiver<&str>, tx2: mpsc::Sender<&str>, args: Args) {
             Ok("PAUSE") => {
                 is_running = !is_running;
                 if !is_running && pl {
-                    tx2.send("LAP").unwrap();
+                    tx.send("LAP").unwrap();
                 }
             }
             Ok("LAP") => {
@@ -81,7 +81,6 @@ fn loop_thread(rx: mpsc::Receiver<&str>, tx2: mpsc::Sender<&str>, args: Args) {
         }
 
         // end of loop
-        thread::sleep(Duration::from_millis(1));
         if is_running {
             print!("\rTime: {}s", total_time.as_millis() as f32 / 1000f32);
             io::stdout().flush().unwrap();
@@ -89,15 +88,3 @@ fn loop_thread(rx: mpsc::Receiver<&str>, tx2: mpsc::Sender<&str>, args: Args) {
         }
     }
 }
-
-//pub fn sw_flags(flags: &mut ApiFlags, args: Vec<String>) {
-//    for arg in &args {
-//        match arg as &str {
-//            "-pl" | "--pause-lap" => flags.pl = true,
-//            _ => {
-//                println!("Please give a valid command for sw. See available commands with -h sw");
-//                process::exit(1);
-//            }
-//        }
-//    }
-//}
